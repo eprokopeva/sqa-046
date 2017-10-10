5ff230606e26f5e7322675e13e2f8e86718dc6e8
omsk43 <omsk43@ptc.local>
2017-10-10 14:06:07 +0600

    Create new module

---
+++ sandbox/src/main/java/com/luxoft/sqa/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.luxoft.sqa;
+
+public class HelloWorld {
+
+public static void main(String[] args){
+    Point p1 = new Point(1,3);
+    Point p2 = new Point(5,6);
+    System.out.println(p1.distance(p2));
+}
+
+
+}
